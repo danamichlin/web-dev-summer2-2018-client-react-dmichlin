3290e2e5963d4ac70465f47e93dfd77312ba2243
Dana <danammichlin@gmail.com>
2018-07-14 21:04:44 -0400

    added files from class

---
+++ src/containers/CourseList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class CourseList extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <h2>Course List</h2>
+        )
+    }
+}
+export default CourseList;
